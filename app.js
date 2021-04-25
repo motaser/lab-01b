90031d8ec159132d91f2e1f12174bca1d66338e2
motaser <>
2021-04-25 18:42:32 +0000

    creatlab01

---
+++ app.js
@@ -0,0 +1,6 @@
+let name =prompt('what is your name ?');
+let live =prompt('where are you live ?');
+let work =prompt('what is your work ?');
+let sport =prompt('what is your favourite sport ?');
+
+alert( 'hello ' + name+ " you are live in " + live+ ' and your work is ' + work+ ' and you love ' +sport);
